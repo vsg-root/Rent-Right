6011949c32f5d40b1f1d522c240d494ed548cf44
heitorleonny <heitorleonnyyt1234@gmail.com>
2023-10-20 07:33:27 -0300

    Criação da branch dashboard onde será trabalhada as análises do dataset

---
+++ home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title = 'Dasboard Rent Right',
+    layout = 'wide',
+    menu_items = {
+        'About': '''Esse projeto foi desenvolvido pelos alunos Alexandre, Heitor, Pedro, Vinivius Ferraz, Vinicius Santos e Vinicius Gomes.'''
+    }
+)
+
+st.markdown(f'''
+    <h1>Hello World</h1>  
+    <p>Esse projeto foi desenvolvido pelos alunos Alexandre, Heitor, Pedro, Vinivius Ferraz, Vinicius Santos e Vinicius Gomes.</p>  
+''', unsafe_allow_html=True)
+
