4272a370343c457ce8a4d96d8c2db60d3f754a45
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2023-10-28 20:10:31 -0300

    feat: added grey box in the background

--- login_interface/lib/main.dart
+++ login_interface/lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
+import 'BackgroundLoginScreen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        width: 270,
-        height: 360,
-        color: Color(0XFFFFFFFF),
-      ),
-    ),
-  ));
   runApp(Background());
-}
-
-class Background extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      color: Color(0XFF213644),
-    ));
-  }
 }
