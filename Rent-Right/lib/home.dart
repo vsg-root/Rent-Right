bd7291390edbfeff2fd94a1a7e70af6ef404dfc5
Vinícius Santos <viniciussantoslima2003@gmail.com>
2023-11-13 01:08:20 -0300

    Merge branch 'app_development' into Marco_1

---
+++ Rent-Right/lib/home.dart
@@ -0,0 +1,188 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'login.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF213644),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 27, vertical: 45),
+        clipBehavior: Clip.antiAlias,
+        decoration: const BoxDecoration(color: Color(0xFF213644)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _buildHeader(),
+            const SizedBox(height: 74),
+            Expanded(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                child: _buildOptions(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Container(
+      width: double.infinity,
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: 176,
+            height: 88,
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  child: Text(
+                    'Bem-vindo',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 29.29,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 73,
+                  child: Text(
+                    'Como podemos te ajudar hoje?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12.20,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 28,
+                  child: Text(
+                    'Rent Right',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35.15,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: 70,
+            height: 70,
+            child: IconButton(
+                iconSize: 60,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      const Color.fromARGB(0, 255, 255, 255)),
+                ),
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  'assets/img/user.svg',
+                  width: 100,
+                  height: 100,
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildOptions() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+            child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              _buildOption('Pesquisar', 'assets/img/dollar-coin.svg', () {}),
+              const SizedBox(width: 32),
+              _buildOption('Recomendações', 'assets/img/eyeball.svg', () {}),
+            ])),
+        const SizedBox(height: 32),
+        Expanded(
+            child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+              _buildOption('Configurações', 'assets/img/cog.svg', () {}),
+              const SizedBox(width: 32),
+              _buildOption('Sair', 'assets/img/logout.svg',
+                  () => Navigator.pop(context)),
+            ])),
+      ],
+    );
+  }
+
+  Widget _buildOption(String btnText, String svgAsset, void Function() onTap) {
+    return Expanded(
+        child: Container(
+      height: double.infinity,
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        color: Color(0xFFD9D9D9),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              svgAsset,
+              width: 50,
+              height: 50,
+            ),
+            const SizedBox(height: 15),
+            Text(
+              btnText,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Color(0xFF0E2433),
+                fontSize: 14,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
---
+++ Rent-Right/lib/login.dart
@@ -0,0 +1,307 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'register.dart';
+import 'retrieve.dart';
+import 'home.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF213644),
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 100,
+          horizontal: 50,
+        ),
+        clipBehavior: Clip.antiAlias,
+        decoration: const BoxDecoration(color: Color(0xFF213644)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Stack(
+                alignment: AlignmentDirectional.topCenter,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 50),
+                      Expanded(
+                        child: Container(
+                          constraints: const BoxConstraints(
+                            minWidth: 275,
+                            minHeight: 362,
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                              begin: Alignment(0.50, -0.87),
+                              end: Alignment(-0.5, 0.87),
+                              colors: [Color(0xBFBEBEBE), Color(0xFCD9D9D9)],
+                            ),
+                            shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(24),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color(0x1F000000),
+                                blurRadius: 6,
+                                offset: Offset(7.50, 8),
+                                spreadRadius: 0,
+                              ),
+                            ],
+                          ),
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                const SizedBox(height: 50),
+                                _buildInputField(
+                                  'Username',
+                                  'assets/img/user.svg',
+                                  TextInputType.text,
+                                  (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter a username';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                const SizedBox(height: 20),
+                                _buildInputField(
+                                  'Password',
+                                  'assets/img/pswd.svg',
+                                  TextInputType.visiblePassword,
+                                  (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Please enter a password';
+                                    }
+                                    return null;
+                                  },
+                                  obscureText: true,
+                                ),
+                                const SizedBox(height: 20),
+                                _buildTextButton('Forgot your password?', () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const RetrievePage()),
+                                  );
+                                }),
+                                const SizedBox(height: 20),
+                                _buildElevatedButton('Login', () {
+                                  _submitForm();
+                                }),
+                                const SizedBox(height: 20),
+                                _buildElevatedButton('Sign Up', () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => RegisterPage()),
+                                  );
+                                }),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // WHITE FRAME IMG
+                  _buildWhiteFrameImage()
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWhiteFrameImage() {
+    return Container(
+      width: 105,
+      height: 105,
+      child: Stack(
+        children: [
+          Align(
+            alignment: AlignmentDirectional.center,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                  color: Color(0xFF0E2433), shape: BoxShape.circle),
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/img/login.svg',
+              width: 100,
+              height: 100,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildInputField(String hintText, String svgAsset,
+      TextInputType inputType, String? Function(String?) validator,
+      {bool obscureText = false}) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        color: const Color(0xFFD9D9D9),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 50,
+            height: double.infinity,
+            padding: const EdgeInsets.all(10),
+            decoration: const BoxDecoration(
+              color: const Color(0xFF0E2433),
+            ),
+            child: Align(
+              alignment: AlignmentDirectional.center,
+              child: SvgPicture.asset(
+                svgAsset,
+                width: 100.0,
+                height: 100.0,
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: TextFormField(
+              validator: validator,
+              keyboardType: inputType,
+              obscureText: obscureText,
+              enableSuggestions: false,
+              autocorrect: false,
+              style: const TextStyle(
+                color: Color.fromARGB(192, 0, 0, 0),
+                fontSize: 16,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w200,
+              ),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: hintText,
+                hintStyle: const TextStyle(
+                  color: Color.fromARGB(192, 0, 0, 0),
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextButton(String buttonText, void Function() onPressed) {
+    return Container(
+      width: double.infinity,
+      height: 25,
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: onPressed,
+            child: Text(
+              buttonText,
+              textAlign: TextAlign.right,
+              style: const TextStyle(
+                color: Color.fromARGB(192, 0, 0, 0),
+                fontSize: 13,
+                fontStyle: FontStyle.italic,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w200,
+                height: 0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildElevatedButton(String buttonText, void Function() onPressed) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        clipBehavior: Clip.antiAlias,
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(const Color(0xFF0E2433)),
+          overlayColor: MaterialStateProperty.resolveWith<Color?>(
+            (Set<MaterialState> states) {
+              if (states.contains(MaterialState.pressed)) {
+                return const Color.fromARGB(255, 28, 71, 100);
+              }
+              return null;
+            },
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          buttonText,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontFamily: 'Inter',
+            fontWeight: FontWeight.w200,
+            height: 0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Welcome!'),
+        ),
+      );
+    }
+  }
+}
