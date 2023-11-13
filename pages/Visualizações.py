015c7820667ccf7a3027cc6bb558517cac7c479e
Alexandre Vital <alexandre.vital@ufrpe.br>
2023-11-13 09:39:12 -0300

    Update Visualizações.py

--- pages/Visualizações.py
+++ pages/Visualizações.py
@@ -6,9 +6,13 @@ import matplotlib.pyplot as plt
 
 df = load_data("dataset\housing.parquet")  # Caminho atualizado
 
+# Selecionando colunas específicas (ajuste conforme necessário)
+selected_columns = ['price', 'sqfeet', 'beds', 'baths', 'cats_allowed', 'dogs_allowed']  # Exemplo
+df_selected = df[selected_columns]
+
 if st.checkbox("Mostrar Mapa de Calor de Correlação"):
     st.write("Mapa de Calor de Correlação")
-    correlation_matrix = df.corr()
-    plt.figure(figsize=(10, 5))
+    correlation_matrix = df_selected.corr()
+    plt.figure(figsize=(15, 10))  # Tamanho ajustado
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
     st.pyplot(plt)
