054e515a6b29c23f499b93d5491f2d7c083963f6
heitorleonny <heitorleonnyyt1234@gmail.com>
2024-02-27 21:06:30 -0300

    update

--- pages/8_Mapa Geoespacial.py
+++ pages/8_Mapa Geoespacial.py
@@ -8,14 +8,14 @@ st.title("Mapa de Calor de Residências")
 
 df = pd.read_csv('model\model_input_data.csv')
 
-# Criar um mapa centrado em uma localização inicial
+
 map_center = [df['lat'].mean(), df['long'].mean()]
 mymap = folium.Map(location=map_center, zoom_start=4)
 
-# Adicionar camada de calor com base nas coordenadas das residências
+
 heat_data = df[['lat', 'long', 'price']].values.tolist()
 HeatMap(heat_data, radius= 15).add_to(mymap)
 
-# Exibir o mapa no Streamlit
+
 
 folium_static(mymap)
